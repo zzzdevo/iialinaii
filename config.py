2fc6d0e511aeb93d8b3af31fdcb093abb28383cb
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-09-28 21:49:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/zzzdevo/DevoX",
+    "https://github.com/zzzdevo/iialinaii",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
